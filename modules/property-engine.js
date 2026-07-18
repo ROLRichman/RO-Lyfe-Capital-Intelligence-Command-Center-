34955305d0f5cfa73b31e34534e7a85522fa3d6f
Richman | RO'Lyfe <richman@rootoflyfe.com>
2026-07-18 17:16:29 -0400

    Add function to send property details to deal

--- modules/property-engine.js
+++ modules/property-engine.js
@@ -26,3 +26,43 @@ let url =
 window.open(url,"_blank");
 
 }
+
+function sendPropertyToDeal(){
+
+document.getElementById("dealAddress").value =
+document.getElementById("propertyAddress").value;
+
+
+document.getElementById("dealPurchase").value =
+document.getElementById("propertyPurchase").value;
+
+
+document.getElementById("dealARV").value =
+document.getElementById("propertyARV").value;
+
+
+document.getElementById("dealRehab").value =
+document.getElementById("propertyRehab").value;
+
+
+
+document.getElementById("dealWorkspace")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+
+document.getElementById("dealWorkspaceResult").innerHTML =
+
+`
+<h3>
+✅ Property Loaded
+</h3>
+
+<p>
+Ready for Deal Creation
+</p>
+
+`;
+
+}
